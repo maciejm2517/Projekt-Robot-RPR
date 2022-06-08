6db93fd514309e6da8c8dc8cfc9df7a31536f810
Maciej <maciej.konto.1357@gmail.com>
2022-06-08 13:10:45 +0200

    wersja ze zmiennymi

--- python/main.py
+++ python/main.py
@@ -22,12 +22,22 @@ def projekcik():
 
     #inicjacja
 
+    l1=0.42+0.15/2
+    l2=0.5
+    alpha1=np.pi/2
+    offset1=np.pi/2
+    theta1_min=0
+    theta1_max=np.pi
+    d2_min=0
+    d2_max=0.4
+    l3=0.221314
+    theta3_min=0
+    theta3_max= 2 * np.pi
     robot=rtb.DHRobot(
         [
-            rtb.RevoluteDH(d=0.2, alpha=np.pi/2, offset=np.pi/2, qlim=[0,2*np.pi] ),
-            rtb.PrismaticDH(offset=0.1, qlim=[0,0.2]),
-            rtb.RevoluteDH(qlim=[0,2*np.pi], d=0.1),
-            #rtb.RevoluteDH(offset=np.pi/2, alpha=np.pi/2, qlim=[0,0])
+            rtb.RevoluteDH(d=l1, alpha=alpha1, offset=offset1, qlim=[theta1_min, theta1_max]),
+            rtb.PrismaticDH(offset=l2, qlim=[d2_min,d2_max]),
+            rtb.RevoluteDH(qlim=[theta3_min, theta3_max], d=l3),
         ], name="RPR")
 
     print(robot)
